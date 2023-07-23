4a72ce654ceed7873d6a455ddd0e32a3e8f7c40d
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-07-23 15:20:28 -0300

    refactor: decouple sendMail from user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,21 +1,18 @@
-import Mail from '../lib/mail-config';
+import mailQueue from "../lib/queue-config";
 
-exports.createUser = async (req, res) => {
-    const { name, email, password } = req.body;
-
-    const user = {
-        name,
-        email,
-        password
-    }
-
-    //realizando o envio de email
-    await Mail.sendMail({
-        from: 'Queue test <queue@teste.com.br>',
-        to: `${name} <${email}>`,
-        subject: 'Cadastro de usuário',
-        html: `Olá ${name} seja bem-vindo ao nosso sistema! :D`,
-    })
-
-    return res.json(user);
-}   
+export default {
+    async createUser(req, res) {
+        const { name, email, password } = req.body;
+    
+        const user = {
+            name,
+            email,
+            password
+        }
+    
+        //realizando o envio de email atraves do jobRegistration
+        await mailQueue.add({ user });
+    
+        return res.json(user);
+    }     
+} 
