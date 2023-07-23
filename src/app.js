440feefba094110427de31521e1deb0181768c8f
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-07-23 15:13:58 -0300

    feat: import userController

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import express from "express";
-import { createUser } from "./controllers/user.controller";
+import userController from "./controllers/user.controller";
 
 export const app = express();
 
 app.use(express.json());
 
-app.post('/user', createUser);
+app.post('/user', userController.createUser);
